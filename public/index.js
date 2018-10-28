4caf8aabb2c8f5cb7ec53759d1554e8432346ad9
Glitch (chrome-shoemaker) <none>
2018-10-27 23:21:05 -0700

    Randmoly generated dot sizes btwn 1-10px

--- public/index.js
+++ public/index.js
@@ -3,9 +3,13 @@ document.body.onload = makeDots;
 function makeDots() {
   let container = document.getElementById("dot-container");
   console.log(container);
-  for (let i=0; i < 50; i++) {
+  for (let i=0; i < 10; i++) {
     let newDot = document.createElement("div");
     newDot.className = "new-dot";
+    let dotSize = (Math.floor(Math.random() * 10) + 1);
+    console.log(dotSize);
+    newDot.style.height = dotSize + "px";
+    newDot.style.width = dotSize + "px";
     container.appendChild(newDot);
   }
 }
