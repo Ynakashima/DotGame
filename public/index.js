3f7c825530accd8e36ce80a3d1f80c1f70923088
Glitch (chrome-shoemaker) <none>
2018-10-28 16:56:55 -0700

    Dot starting point x axis is random

--- public/index.js
+++ public/index.js
@@ -12,6 +12,11 @@ function dotGeneration() {
       console.log(dotSize)
       newDot.style.height = dotSize + "px"
       newDot.style.width = dotSize + "px"
+
+      let startPoint = (Math.floor(Math.random() * 95) + 1);
+      newDot.style.left = startPoint + "%";
+      newDot.style.top = "0";
+
       container.appendChild(newDot);
     // }
 
