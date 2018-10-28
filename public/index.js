f1685facccae73e32d03883d27a7885a70737d8d
Glitch (chrome-shoemaker) <none>
2018-10-28 01:32:25 -0700

    Dots appear at 1000ms intervals via setTimeout

--- public/index.js
+++ public/index.js
@@ -1,18 +1,26 @@
-document.body.onload = makeDots;
+document.body.onload = dotGeneration;
   
-function makeDots() {
-  let container = document.getElementById("dot-container");
-  console.log(container);
-  for (let i=0; i < 10; i++) {
-    let newDot = document.createElement("div");
-    newDot.className = "new-dot";
-    let dotSize = (Math.floor(Math.random() * 10) + 1);
-    console.log(dotSize);
-    newDot.style.height = dotSize + "px";
-    newDot.style.width = dotSize + "px";
-    container.appendChild(newDot);
+function dotGeneration() {
+  function makeDots() {
+    let container = document.getElementById("dot-container");
+
+    // for (let i=0; i < 2; i++) {
+      let newDot = document.createElement("div")
+      newDot.className = "new-dot"
+
+      let dotSize = (Math.floor(Math.random() * 100) + 10)
+      console.log(dotSize)
+      newDot.style.height = dotSize + "px"
+      newDot.style.width = dotSize + "px"
+      container.appendChild(newDot);
+    // }
+
   }
+
+  setInterval(makeDots, 1000);
 }
+
+
 
 
   
